f2a3564033cd59cb44261553c087f11f31ebac82
kozadaev22 <kozadaev22@mail.ru>
2023-07-09 22:18:42 +0300

    Add allure

--- src/test/java/TestFormsCard.java
+++ src/test/java/TestFormsCard.java
@@ -1,6 +1,8 @@
-import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Configuration;
-import org.junit.jupiter.api.BeforeEach;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.Keys;
@@ -13,10 +15,20 @@ import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
 public class TestFormsCard {
+    @BeforeAll
+    static void setUpAll() {
+        SelenideLogger.addListener("allure", new AllureSelenide());
+    }
+
+    @AfterAll
+    static void tearDownAll() {
+        SelenideLogger.removeListener("allure");
+    }
+
     @Test
     @DisplayName("Should successful plan and replan meeting")
     void shouldSuccessfulPlanAndReplanMeeting() {
-//        Configuration.holdBrowserOpen = true;
+        Configuration.holdBrowserOpen = true;
 //        - Чтобы после окончания не закрылась
         open("http://localhost:9999");
         var validUser = DataGenerator.Registration.generateUser("ru");
